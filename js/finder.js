71c23a01e654038cc9c47adc966a1aafeb0a9de6
Rick Stoner <stonerrick19@gmail.com>
2016-01-25 15:16:27 -0800

    added sort function, compare ingredients functions, working on display function

--- js/finder.js
+++ js/finder.js
@@ -1,21 +1,55 @@
 'use strict';
-var userIngredients = ['vodka','orange juice', 'whiskey']
+
+var userIngredients = ['orange juice','vodka','whiskey'];
 
 
-function compareIngredients(userIngredients,drinks){
-  // for(var l; l < drinks.length; l++){
-  //   drinks[l].percentMatch = 0;
-  // }
-  for(var i; i < userIngredients.length; i++){
-      for(var j; j < drinks.length; j++){
-        for(var k; k < drinks[j].ingredients.length; k++){
-          if(userIngredients[i].toLowerCase() === drinks[j].ingredients[k][0].toLowerCase()){
-            drinks[j].match =+ 1;
+function compareIngredients(ingredients,drinks){
+  for(var l; l < drinks.length; l++){
+    drinks[l].percentMatch = 0;
+  }
+  var sort = [];
+  for(var i = 0; i < drinks.length; i++){
+      for(var j = 0; j < ingredients.length; j++){
+        for(var k = 0; k < drinks[i].ingredients.length; k++){
+          if(ingredients[j].toLowerCase() === drinks[i].ingredients[k][0].toLowerCase()){
+            drinks[i].match += 1;
           }
         }
-        drinks[j].percentMatch = drinks[j].match / drinks[j].ingredients.length;
+        drinks[i].percentMatch = drinks[i].match / drinks[i].ingredients.length;
       }
   }
 }
+compareIngredients(userIngredients, drinks);
 
-compareIngredients();
+var sortedResults = sortByMatch(drinks);
+
+function sortByMatch(drinks){
+  var drinkSort = [];
+  for(var i = 0; i < drinks.length; i++){
+    drinkSort.push([i,drinks[i].percentMatch]);
+  }
+  drinkSort.sort(function(a,b){return b[1]-a[1]});
+  return drinkSort;
+}
+
+function displayResults(sortedResults,drinks){
+  var drinkDisplay = document.getElementById('drinkResults');
+  console.log(drinkDisplay);
+  var resultArticle = document.createElement('article');
+  for (var i =0; i < sortedResults.length; i++){
+    resultArticle.setAttribute("id", 'result' + i);
+    drinkDisplay.appendChild(resultArticle);
+    var drinkItems = document.getElementById('result'+i);
+    var imageEl = document.createElement('img');
+    var headingEl = document.createElement('h4');
+    imageEl.src='img/'+ drinks[sortedResults[i][0]].imageProperty;
+    headingEl.textContent = drinks[sortedResults[i][0]].name;
+    drinkItems.appendChild(headingEl);
+    drinkItems.appendChild(imageEl);
+  }
+}
+displayResults(sortedResults,drinks);
+
+
+// ingMatch
+// ingMismatch
