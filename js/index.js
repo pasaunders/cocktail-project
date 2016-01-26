253b86211d53cd2cd9735034c8c74ca404ae7a9e
pasaunders <kaorti@gmail.com>
2016-01-25 20:45:41 -0800

    Working on the ingredient list input function. Text box and submit button work, but I'm not getting the data from the text box into the function and split into seperate strings.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var submitIngredients = document.getElementById('submitIngredients');
-submitIngredients.addEventListener('submit', enterIngredientSubmit);
+var ingredientsEntry = document.getElementById('ingredients-entry');
+ingredientsEntry.addEventListener('submit', enterIngredientSubmit); //this should listen for the submit button, then grab and send the text box contents to the function.
 
-function enterIngredientSubmit(event) {
+function enterIngredientSubmit(event) {  //am I properly passing information thorugh here?
   console.log(event);
   event.preventDefault();
-  var ingredientList = event.split(',');
-  console.log(ingredientList);
+  var ingredientsAvailable = ingredientList.split(','); //this should take the text box entry and split it at each comma into individual strings.
+  console.log(ingredientsAvailable);
 }
