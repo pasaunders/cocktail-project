45782a986b7bf9607a27191ce846f4e070989b53
pasaunders <kaorti@gmail.com>
2016-01-25 17:20:40 -0800

    Revert "beginning of the ingredient entry code. Incomplete."

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-var submitIngredients = document.getElementById('submitIngredients');
-submitIngredients.addEventListener('submit', enterIngredientSubmit);
-
-function enterIngredientSubmit(event) {
-  console.log(event);
-  event.preventDefault();
-  var ingredientList = event.split(',');
-  console.log(ingredientList);
+function enterIngredient() {
+  document.getElementById('id') //id to be determined
 }
