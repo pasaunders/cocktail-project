03091252f5808f32dd241f7a7d412f7e5f38ea97
pasaunders <kaorti@gmail.com>
2016-01-26 12:17:22 -0800

    entering and saving the user's ingredient list works. First steps on entering new recipies.

--- js/index.js
+++ js/index.js
@@ -4,8 +4,15 @@ var ingredientsEntry = document.getElementById('ingredients-entry');
 ingredientsEntry.addEventListener('submit', enterIngredientSubmit); //this should listen for the submit button, then grab and send the text box contents to the function.
 
 function enterIngredientSubmit(event) {  //am I properly passing information thorugh here?
-  console.log(event);
+  console.log(event.target.ingredientList.value);
   event.preventDefault();
-  var ingredientsAvailable = ingredientList.split(','); //this should take the text box entry and split it at each comma into individual strings.
+  var ingredientsAvailable = event.target.ingredientList.value.split(', '); //this should take the text box entry and split it at each comma into individual strings.
+  event.target.ingredientList.value = null;
   console.log(ingredientsAvailable);
+  for (var i = 0; i < ingredientsAvailable.length; i++) {
+    var newIngredient = new ingredient(ingredientsAvailable[i]);
+    console.log(ingArray);
+    ingArray.push(newIngredient);
+  }
+  updateIngredients();
 }
